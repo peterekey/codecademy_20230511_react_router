d2018dcd03d7b780dffca383c9ca095a05aa7423
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-13 09:09:31 +1000

    7/12, Task 1: Import useParams to Author component

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectAuthors } from "../features/authors/authorsSlice";
-import { Link } from "react-router-dom";
-
-// Import useParams from React Router
+import { Link, useParams } from "react-router-dom";
 
 export default function Author () {
 
