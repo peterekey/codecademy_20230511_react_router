c21fd7e7be5c8dabc642b5000ce6b3412409e472
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-13 07:59:51 +1000

    4/12, Task 3: Ensure routes are working (added import Provider from react-redux)

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -9,16 +9,21 @@ import Author from "../components/Author";
 import Profile from "../components/Profile";
 import EditProfileForm from "../components/EditProfileForm";
 import Root from "../components/Root";
+import '../app/App.css'
 
-import { createBrowserRouter, createRouteFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
 import "./App.css";
 
-const router = 
-  /* Wrap this Root Route to create Router here */
-  <Route path="/" element={ <Root/> }>
-    {/* Add Routes here! */}
-  </Route>
+const router = createBrowserRouter( createRoutesFromElements(
+    <Route path="/" element={ <Root/> }>
+        <Route path="about" element={<About />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="articles" element={<Articles />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="profile" element={<Profile />} />
+      // </Route>
+  ))
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App.jsx'
+import { store } from "./app/store.jsx";
+import { Provider } from "react-redux";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
   </React.StrictMode>,
 )
