2e3f472d4afd216d43a83edffa5d5f69623459dc
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-13 08:16:10 +1000

    5/12, Task 1: Replace anchor tags with Link components. Also fix references in store.jsx

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import articlesReducer from '../../starting_code/src/features/articles/articlesSlice';
-import categoriesReducer from '../../starting_code/src/features/categories/categoriesSlice';
-import sessionReducer from '../../starting_code/src/features/session/sessionSlice';
-import authorsReducer from '../../starting_code/src/features/authors/authorsSlice';
+import articlesReducer from '../../src/features/articles/articlesSlice';
+import categoriesReducer from '../../src/features/categories/categoriesSlice';
+import sessionReducer from '../../src/features/session/sessionSlice';
+import authorsReducer from '../../src/features/authors/authorsSlice';
 
 export const store = configureStore({
   reducer: {
