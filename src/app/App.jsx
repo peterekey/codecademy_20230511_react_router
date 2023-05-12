88a9f35facd13bfad1a77f0ae5fa444e97991696
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-13 09:22:24 +1000

    Fix typo in sign-up route that was giving a 404 error

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter( createRoutesFromElements(
         <Route path="articles/:title" element={ <Article />}/>
         <Route path="authors/:name" element={ <Author />}/>
         <Route path="about" element={<About />} />
-        <Route path="signup" element={<SignUp />} />
+        <Route path="sign-up" element={<SignUp />} />
         <Route path="articles" element={<Articles />} />
         <Route path="categories" element={<Categories />} />
         <Route path="profile" element={<Profile />} />
