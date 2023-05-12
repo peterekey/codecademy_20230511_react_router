3e0e760af04a5f9d740445eddaf4a8d39592345e
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-13 09:10:50 +1000

    7/12, Task 2: Use destructurcing syntax to extract :name value

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -6,6 +6,7 @@ import { Link, useParams } from "react-router-dom";
 export default function Author () {
 
   // Extract the `name` URL parameter.
+  let { name } = useParams()
 
   const authors = useSelector(selectAuthors)
   const author = authors["replace me"]
