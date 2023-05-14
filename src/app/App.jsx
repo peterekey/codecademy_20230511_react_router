84528344ff2485d6eb45f51ec2e972050df681f2
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-15 07:53:52 +1000

    8/12, Task 3: Define child route for the Category route

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -22,7 +22,9 @@ const router = createBrowserRouter( createRoutesFromElements(
         <Route path="about" element={<About />} />
         <Route path="sign-up" element={<SignUp />} />
         <Route path="articles" element={<Articles />} />
-        <Route path="categories" element={<Categories />} />
+        <Route path="categories" element={<Categories />}>
+          <Route path=":name" element={<Category />} />
+        </Route>
         <Route path="profile" element={<Profile />}>
           <Route path="edit" element={<EditProfileForm />} />
         </Route>
